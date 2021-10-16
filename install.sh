8519770150d81bd6ffa5cdbaa928d0d97acca8d4
flunkyDoge <jtylerbond4@gmail.com>
2021-10-16 16:04:14 -0700

    added fd variable for package manager compatability

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo
+
 # For Yes and No inputs
 function yes_or_no {
     while true; do
@@ -11,6 +12,9 @@ function yes_or_no {
         esac
     done
 }
+
+fd=$fd
+
 # Detect package manager and set 'install' to packager manager install command
 if test -f '/etc/arch-release'; then
   echo "Package manager detected: pacman"
@@ -22,6 +26,7 @@ elif test -f '/etc/debian_version'; then
   echo "Package manager detected: apt-get"
   install=$'sudo apt-get install'
   noconfirm=$'-y'
+  fd=$fdclone
   sudo apt-get update
   echo
 elif test -f '/etc/redhat-release'; then
@@ -37,17 +42,12 @@ elif test -f '/etc/SuSE-release'; then
   install=$'sudo zypper install'
   echo
 fi
-# Detects if neovim is installed
-echo 'Checking if neovim is installed for obvious reasons'
-if test -f '/usr/bin/nvim'; then
-  echo 'Neovim is already installed!'
-  echo
-else
-  question='Neovim is not installed, would you like to install it with required dependencies (fd, lua, lolcat, ranger) now?'
-  abort='Neovim and dependencies'
-  $install $noconfirm git neovim fd lua lolcat ranger
-  echo ''
-fi
+
+question='Neovim is not installed, would you like to install it with required dependencies (fd, lua, lolcat, ranger) now?'
+abort='Neovim and dependencies'
+$install $noconfirm git neovim $fd lua lolcat ranger
+echo
+
 # Detects if npm is installed
 echo "Checking if npm is installed, this is used for installing language servers"
 if test -f '/usr/bin/npm'; then
@@ -57,10 +57,12 @@ else
   abort='npm'
   $install npm $noconfirm
 fi
+
 # Installs language servers
 echo 'Installing required language servers'
 sudo npm i pyright bash-language-server
 echo
+
 # Here begins the installation of the actual config
 if test -f '~/.config/nvim'; then
   question='Neovim config found! Would you like to overwrite and create a backup (~/nvim-back)'
@@ -74,5 +76,7 @@ else
   git clone https://github.com/flunkyDoge/nvim-config.git ~/.config/nvim
   echo
 fi
+
 nvim -c PlugInstall -c q! -c q!
+
 echo 'My Neovim config is installed!'
