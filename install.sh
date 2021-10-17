ddcf5156ca7de76e91567916b5ff43516f8b872f
flunkyDoge <jtylerbond4@gmail.com>
2021-10-16 17:57:10 -0700

    fixed progress bar

--- install.sh
+++ install.sh
@@ -4,138 +4,132 @@ echo
 
 fd=$fd
 
-echo '
-Progress... [...................................................] (00%)'
+echo -ne '
+Progress... [...................................................] (00%)'\r
+sleep 0.1
+echo -ne 'Detecting package manager
+Progress... [#..................................................] (02%)'\r
 sleep 0.1
 echo 'Detecting package manager
-Progress... [#..................................................] (02%)'
-sleep 0.1
-echo 'Detecting package manager
-Progress... [##.................................................] (04%)'
+Progress... [##.................................................] (04%)'\r
 # Detect package manager and set 'install' to packager manager install command
 if test -f '/etc/arch-release'; then
-  echo "Package manager detected: pacman
-Progress... [####...............................................] (08%)"
+  echo -ne "Package manager detected: pacman
+Progress... [####...............................................] (08%)"\r
   install=$'sudo pacman -S'
   noconfirm=$'--noconfirm'
   sudo pacman -Syu --noconfirm > /dev/null
-  echo
 elif test -f '/etc/debian_version'; then
-  echo "Package manager detected: apt-get
-Progress... [####...............................................] (08%)"
+  echo -ne "Package manager detected: apt-get
+Progress... [####...............................................] (08%)"\r
   install=$'sudo apt-get install'
   noconfirm=$'-y'
   fd=$fdclone
   sudo apt-get upgrade > /dev/null
-  echo
 elif test -f '/etc/redhat-release'; then
-  echo "Package manager detected: yum"
+  echo -ne "Package manager detected: yum"
   install=$'sudo yum install'
-  echo
 elif test -f '/etc/gentoo-release'; then
-  echo "Package manager detected: emerge"
+  echo -ne "Package manager detected: emerge"
   install=$'sudo emerge install'
-  echo
 elif test -f '/etc/SuSE-release'; then
-  echo "Package manager detected: zypper"
+  echo -ne "Package manager detected: zypper"
   install=$'sudo zypper install'
-  echo
 fi
 
 sleep 0.25
-echo 'Set install variables
-Progress... [######.............................................] (12%)'
+echo -ne 'Set install variables
+Progress... [######.............................................] (12%)'\r
 sleep 0.25
 
-echo 'Installing Neovim and dependencies
-Progress... [########...........................................] (16%)'
+echo -ne 'Installing Neovim and dependencies
+Progress... [########...........................................] (16%)'\r
 $install $noconfirm git neovim $fd lolcat ranger > /dev/null
-echo
 
 sleep 0.1
-echo 'Installing Neovim and dependencies
-Progress... [##########.........................................] (20%)'
+echo -ne 'Installing Neovim and dependencies
+Progress... [##########.........................................] (20%)'\r
 sleep 0.1
-echo 'Installing Neovim and dependencies
-Progress... [##############.....................................] (26%)'
+echo -ne 'Installing Neovim and dependencies
+Progress... [##############.....................................] (26%)'\r
 sleep 0.1
-echo 'Dependencies installed
-Progress... [################...................................] (30%)'
+echo -ne 'Dependencies installed
+Progress... [################...................................] (30%)'\r
 
 # Detects if npm is installed
-echo "Checking if npm is installed, this is used for installing language servers
-Progress... [##################.................................] (35%)"
+echo -ne "Checking if npm is installed, this is used for installing language servers
+Progress... [##################.................................] (35%)"\r
 sleep 0.25
-echo "Checking if npm is installed, this is used for installing language servers
-Progress... [####################...............................] (38%)"
+echo -ne "Checking if npm is installed, this is used for installing language servers
+Progress... [####################...............................] (38%)"\r
 if test -f '/usr/bin/npm'; then
-  echo 'Npm is already installed
-Progress... [######################.............................] (42%)'
+  echo -ne 'Npm is already installed
+  Progress... [######################.............................] (42%)'\r
   sleep 0.15
-  echo 'Npm is already installed
-Progress... [########################...........................] (46%)'
+  echo -ne 'Npm is already installed
+  Progress... [########################...........................] (46%)'\r
 else
   sleep 0.25
-  echo 'Npm is not installed, installing
-Progress... [######################.............................] (42%)'
+  echo -ne 'Npm is not installed, installing
+  Progress... [######################.............................] (42%)'\r
   $install npm $noconfirm > /dev/null
-  echo 'Npm successfuly installed
-Progress... [########################...........................] (46%)'
+  echo -ne 'Npm successfuly installed
+  Progress... [########################...........................] (46%)'\r
 fi
 
 sleep 0.2
-echo 'Npm is installed
-Progress... [#########################..........................] (48%)'
+echo -ne 'Npm is installed
+Progress... [#########################..........................] (48%)'\r
 sleep 0.15
-echo 'Installing required language servers
-Progress... [##########################.........................] (50%)'
+echo -ne 'Installing required language servers
+Progress... [##########################.........................] (50%)'\r
 # Installs language servers
-echo 'Installing required language servers
-Progress... [#############################......................] (56%)'
+echo -ne 'Installing required language servers
+Progress... [#############################......................] (56%)'\r
 sudo npm i pyright bash-language-server > /dev/null
-echo 'Language servers successfuly installed
-Progress... [################################...................] (62%)'
+echo -ne 'Language servers successfuly installed
+Progress... [################################...................] (62%)'\r
 sleep 0.2 
-echo 'Language servers successfuly installed
-Progress... [###################################................] (68%)'
+echo -ne 'Language servers successfuly installed
+Progress... [###################################................] (68%)'\r
 sleep 0.15
-echo 'Checking for pre-existing ~/.config/nvim 
-Progress... [######################################.............] (74%)'
+echo -ne 'Checking for pre-existing ~/.config/nvim 
+Progress... [######################################.............] (74%)'\r
 sleep 0.2
-echo 'Checking for pre-existing ~/.config/nvim 
-Progress... [########################################...........] (78%)'
+echo -ne 'Checking for pre-existing ~/.config/nvim 
+Progress... [########################################...........] (78%)'\r
 sleep 0.1
 
 # Here begins the installation of the actual config
 if test -f '~/.config/nvim'; then
-  echo 'Nvim config found, deleting
-Progress... [##########################################.........] (82%)'
+  echo -ne 'Nvim config found, deleting
+  Progress... [##########################################.........] (82%)'\r
   rm -r ~/.config/nvim
   sleep 0.15
-  echo 'Nvim config deleted, cloning Nvim config
-Progress... [############################################.......] (86%)'
+  echo -ne 'Nvim config deleted, cloning Nvim config
+  Progress... [############################################.......] (86%)'\r
   git clone https://github.com/flunkyDoge/nvim-config.git ~/.config/nvim
-  echo 'Nvim config successfuly cloned
-Progress... [##############################################.....] (90%)'
+  echo -ne 'Nvim config successfuly cloned
+  Progress... [##############################################.....] (90%)'\r
 else
-  echo 'No Nvim config found
-Progress... [##########################################.........] (82%)'
+  echo -ne 'No Nvim config found
+  Progress... [##########################################.........] (82%)'\r
 sleep 0.15
-  echo 'Cloning Neovim config 
-Progress... [############################################.......] (86%)'
+  echo -ne 'Cloning Neovim config 
+  Progress... [############################################.......] (86%)'\r
   git clone https://github.com/flunkyDoge/nvim-config.git ~/.config/nvim
-  echo 'Nvim config successfuly cloned
-Progress... [##############################################.....] (90%)'
+  echo -ne 'Nvim config successfuly cloned
+  Progress... [##############################################.....] (90%)'\r
 fi
 sleep 0.2 
-echo 'Nvim config successfuly cloned 
-Progress... [###############################################....] (92%)'
+echo -ne 'Nvim config successfuly cloned 
+Progress... [###############################################....] (92%)'\r
 sleep 0.15
-echo 'Installing plugins for Nvim
-Progress... [################################################...] (94%)'
+echo -ne 'Installing plugins for Nvim
+Progress... [################################################...] (94%)'\r
 nvim -c PlugInstall -c q! -c q!
-echo 'Plugins installed!
-Progress... [##################################################.] (98%)'
+echo -ne 'Plugins installed!
+Progress... [##################################################.] (98%)'\r
 
-echo 'Neovim config is installed!
-Progress... [###################################################] (100%)'
+echo -ne 'Neovim config is installed!
+Progress... [###################################################] (100%)'\r
