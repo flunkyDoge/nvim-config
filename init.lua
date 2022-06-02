baebd8037b6c448f5c4d60e3408784161541cb12
flunkyDoge <jtylerbond4@gmail.com>
2022-06-02 10:34:52 -0700

    fixed csharp lsp

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ require('colorscheme')
 require('lsp.python-lsp')
 require('lsp.lua-lsp')
 require('lsp.bash-lsp')
-require('lsp.csharp-lsp')
+-- require('lsp.csharp-lsp')
 
 terminal = require('nvim-terminal').DefaultTerminal;
 
